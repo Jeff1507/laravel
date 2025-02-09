8d1fd2365a7444342c1e2ceef530ff5f11dd8120
JEFF1507 <jeffchaves1507@gmail.com>
2025-02-08 23:03:21 -0300

    estilizacao 3

--- resources/views/clientes/show.blade.php
+++ resources/views/clientes/show.blade.php
@@ -4,37 +4,63 @@
 
 @section('content')
     <div class="flex items-start gap-4">
-        <div class="bg-white p-8 rounded-lg grid grid-cols-2 gap-2 w-[350px]">
+        <div class="bg-white p-8 rounded-lg flex flex-col items-center justify-center gap-2 max-w-max">
             <div>
-                <h3>Nome</h3>
-                <p class="uppercase">{{ $cliente->nome }}</p>
+                <img src="" alt="">
             </div>
-            <div>
+            <h3 class="uppercase">{{ $cliente->nome }}</h3>
+            <div class="flex items-center justify-between gap-2">
                 <h3>CPF</h3>
                 <p>{{ $cliente->cpf }}</p>
-            </div>
-            <div>
+            
                 <h3>Email</h3>
                 <p>{{ $cliente->email }}</p>
-            </div>
-            <div>
+            
                 <h3>Telefone</h3>
                 <p>{{ $cliente->telefone }}</p>
             </div>
         </div>
 
-        <div class="bg-white p-10 rounded-lg space-y-4 flex-1">
-            <h2 class="font-bold text-xl">Endereço do Cliente</h2>
-            <div class="grid grid-cols-3 gap-2">
-                <p>CEP: {{ $cliente->endereco->cep }}</p>
-                <p>Rua: {{ $cliente->endereco->rua }}</p>
-                <p>Bairro: {{ $cliente->endereco->bairro }}</p>
-                <p>Cidade: {{ $cliente->endereco->cidade }}</p>
-                <p>Estado: {{ $cliente->endereco->estado }}</p>
-                <p>numero: {{ $cliente->endereco->numero }}</p>
-                @if($cliente->endereco->complemento)
-                    <p class="text-gray-600"><strong>Complemento:</strong> {{ $cliente->endereco->complemento }}</p>
-                @endif
+        <div class="bg-white p-10 rounded-lg space-y-8 flex-1">
+            <h2 class="font-bold text-2xl text-gray-600">Endereço do Cliente</h2>
+            <div class="grid grid-cols-3 gap-4">
+                <div>
+                    <h3 class="font-light text-sm">CEP</h3>
+                    <p class="text-lg font-medium text-gray-600">{{ $cliente->endereco->cep }}</p>
+                </div>
+                <div>
+                    <h3 class="font-light text-sm">Rua</h3>
+                    <p class="text-lg font-medium text-gray-600">{{ $cliente->endereco->rua }}</p>
+                </div>
+                <div>
+                    <h3 class="font-light text-sm">Bairro</h3>
+                    <p class="text-lg font-medium text-gray-600">{{ $cliente->endereco->bairro }}</p>
+                </div>
+                <div>
+                    <h3 class="font-light text-sm">Cidade</h3>
+                    <p class="text-lg font-medium text-gray-600">{{ $cliente->endereco->cidade }}</p>
+                </div>
+                <div>
+                    <h3 class="font-light text-sm">Estado</h3>
+                    <p class="text-lg font-medium text-gray-600">{{ $cliente->endereco->estado }}</p>
+                </div>
+                <div>
+                    <h3 class="font-light text-sm">Número da Residência</h3>
+                    <p class="text-lg font-medium text-gray-600">{{ $cliente->endereco->numero }}</p>
+                </div>
+
+                
+                <div class="col-span-full">
+                    <h3 class="font-light text-sm">Complemento</h3>
+                    @if($cliente->endereco->complemento)
+                        <p class="text-lg font-medium text-gray-600">{{ $cliente->endereco->complemento }}</p>
+                    @else
+                        <p class="text-lg font-medium text-gray-600">Nenhum complemento foi informado!</p>
+                    @endif
+                </div>
+            </div>
+            <div>
+                <a href="#" class="bg-blue-600 cursor-pointer hover:bg-blue-800 px-4 py-2 text-white font-mediu rounded-lg">Editar Endereço</a>
             </div>
         </div>
     </div>
